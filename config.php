80696285d525a4dd6bc38ec02af3cd03a771dba6
Edward Saxton <eddsax@googlemail.com>
2008-11-25 01:51:14 +0000

    Bad config file. Should be upgraded later.

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+    $databasename = 'tvphp';
+    $databaseuser = 'tvphp';
+    $databasepassword = 'password';
+?>
--- tvlisting.php
+++ tvlisting.php
@@ -3,10 +3,12 @@
     <title>Just testing</title>
 </head>
 <body>
-    <h1>
-    <?php
-    echo "welcome";
-    ?>
-    </h1>
+    <h1><?php
+    require("config.php");
+    echo $databaseuser;
+    ?></h1>
+    <h2><?php
+        echo $databasename;
+    ?></h2>
 </body>
 </html>
