3ded42b2d9299623fca7e4838bf32d28217b2371
Edward Saxton <eddsax@googlemail.com>
2008-12-01 17:00:36 +0000

    Load registered users channel data from the database.

--- models.php
+++ models.php
@@ -4,25 +4,6 @@
     mysql_connect($databaseserver, $databaseuser, $databasepassword);
     mysql_select_db($databasename) or die ("Unable to select database!");
 
-    function get_all_channels(){
-        $query = "SELECT channelName, url, standard, storeddays FROM channel;";
-        $result = mysql_query($query) or die ("Error in query:". $query." ".mysql_error());
-        if (mysql_num_rows($result) > 0) {
-            $answer = array();
-            while($row = mysql_fetch_row($result)) {
-                if ($row[3] >= 0) {
-                    $data = array("ChannelName"=>$row[0], "URL"=>$row[1], "default?"=>$row[2]);
-                    array_push($answer, $data);
-                }
-            }
-            mysql_free_result($result); 
-            return $answer;
-        }
-        else{
-            return array();
-        }
-    }
-
     function load_user(){
         if ($_COOKIE and array_key_exists("validuser", $_COOKIE)){
             return new DBUser($_COOKIE["validuser"]);
@@ -45,18 +26,46 @@
 
     class DBUser extends User{
         public function __construct($name){
-            parent::__construct();
             $this->name = $name;
+            parent::__construct();
         }
 
         public function load_channels(){
-            $this->channels = get_all_channels();
+            $query = "SELECT channelName, state FROM userchannels where username = '".$this->name."'";
+            $result = mysql_query($query) or die ("Error in query:". $query." ".mysql_error());
+            if (mysql_num_rows($result) > 0) {
+                $answer = array();
+                while($row = mysql_fetch_row($result)) {
+                    $data = array("ChannelName"=>$row[0], "URL"=>"", "default?"=>($row[1] == 1));
+                    array_push($answer, $data);
+                }
+                mysql_free_result($result);
+                $this->channels = $answer;
+            }
+            else{
+                $this->channels = array();
+            }
         }
     }
 
     class SessionUser extends User{
         public function load_channels(){
-            $this->channels = get_all_channels();
+            $query = "SELECT channelName, standard, storeddays FROM channel;";
+            $result = mysql_query($query) or die ("Error in query:". $query." ".mysql_error());
+            if (mysql_num_rows($result) > 0) {
+                $answer = array();
+                while($row = mysql_fetch_row($result)) {
+                    if ($row[2] >= 0) {
+                        $data = array("ChannelName"=>$row[0], "URL"=>"", "default?"=>$row[1]);
+                        array_push($answer, $data);
+                    }
+                }
+                mysql_free_result($result);
+                $this->channels = $answer;
+            }
+            else{
+                $this->channels = array();
+            }
             $_SESSION['channels'] = $this->channels;
         }
     }
