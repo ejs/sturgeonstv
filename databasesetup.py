4151b55a1da1b18653928e788fb5ffe132eaa9ed
Edward Saxton <eddsax@googlemail.com>
2008-12-21 20:31:54 +0000

    Fix database creation script.

--- databasesetup.py
+++ databasesetup.py
@@ -1,4 +1,3 @@
-
 #!/Library/Frameworks/Python.framework/Versions/Current/bin/python
 import re
 import feedparser
@@ -22,15 +21,15 @@ data = read_PHP_config_file('config.php')
 db = MySQLdb.connect(host=data['databaseserver'], user=data['databaseuser'], passwd=data['databasepassword'], db=data['databasename'])
 cursor = db.cursor()
 
-
 cursor.execute("""CREATE TABLE channel (
         channelname VARCHAR(20),
         url VARCHAR(90),
         lastchecked DATETIME,
         storeddays INT,
         marker INT,
-        standard TINYINT,
-    );"""
+        daystart TIME,
+        standard TINYINT
+    );""")
 
 cursor.execute("""CREATE TABLE user (
         username VARCHAR(20),
@@ -38,20 +37,20 @@ cursor.execute("""CREATE TABLE user (
         password VARCHAR(40),
         varified TINYINT,
         created DATETIME,
-        lastused DATETIME,
-    );"""
+        lastused DATETIME
+    );""")
 
 cursor.execute("""CREATE TABLE userchannels (
         username VARCHAR(20),
         channelname VARCHAR(20),
         state INT,
-        set_on DATETIME,
-    );"""
+        set_on DATETIME
+    );""")
 
 cursor.execute("""CREATE TABLE tvshow (
         showname VARCHAR(100),
-        average_rating double,
-    );"""
+        average_rating double
+    );""")
 
 cursor.execute("""CREATE TABLE tvshowinstance (
         starttime DATETIME,
@@ -59,12 +58,26 @@ cursor.execute("""CREATE TABLE tvshowinstance (
         showname VARCHAR(100),
         channelname VARCHAR(20),
         discription VARCHAR(200),
-        url VARCHAR(100),
-    );"""
+        url VARCHAR(100)
+    );""")
 
 cursor.execute("""CREATE TABLE tvshowrating (
         username VARCHAR(20),
-        showname VARCAHR(100),
+        showname VARCHAR(100),
         rating INT,
-        lastset DATETIME,
-    );"""
+        lastset DATETIME
+    );""")
+
+for line in open("chanels.txt"):
+    data = line.split()
+    url = data[0]
+    default=data[1]
+    name = ' '.join(data[2:])
+    cursor.execute("""INSERT channel SET
+        channelname="%s",
+        url="%s",
+        lastchecked=NOW(),
+        storeddays=0,
+        marker=0,
+        daystart='06:00:00',
+        standard=%s;"""%(name, url, default))
